bf8933c0402c3e7b02e90f7a9587cb9353ef7dc6
Py7Val <45321489+Py7Val@users.noreply.github.com>
2018-12-30 05:03:54 +0100

    Add files via upload

--- Proyecto.py
+++ Proyecto.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
-# Importamos librerías y definimos variables
+# Importamos librerías
 import RPi.GPIO as GPIO
 import MFRC522
 import signal
@@ -15,12 +15,13 @@ from email import encoders
 from picamera import PiCamera
 from sense_hat import SenseHat
 
+# Definimos variables de leds de Sense Hat y direcciones email para alerta de seguridad
 R = [255, 0, 0]  # Rojo
 G = [0, 255, 0]  # Verde
 direccion_fuente = "ajtv7777777@gmail.com"
 direccion_destino = "ajtv7777777@gmail.com"
 
-# Creamos objeto de Sense Hat y del RFID
+# Creamos objeto de Sense Hat, del RFID y definimos TOKEN e ID de conversación de Telegram
 sense=SenseHat()
 MIFAREReader = MFRC522.MFRC522()
 chatid = '701895569'
@@ -60,9 +61,7 @@ while lectura_continua:
 
     # Si tenemos la ID, nos la imprime por pantalla
     if status == MIFAREReader.MI_OK:
-
         print "Lectura tarjeta User ID: %s,%s,%s,%s" % (uid[0], uid[1], uid[2], uid[3])
-        # Congelamos tiempo 1s
         time.sleep(1)
 
         # Si la tarjeta tiene el UID que buscamos se permite el acceso
@@ -90,20 +89,24 @@ while lectura_continua:
             # Medida de temperatura
             Temp1=sense.get_temperature_from_humidity()
             T1=float(round(Temp1,2))
-            print("La temperatura es de: %.2f\n" % (Temp1))
+            print("La temperatura es de: %.2f" % (Temp1))
             TStr=str(round(Temp1,2))
             sense.show_message("TEMPERATURA: "+TStr, scroll_speed = 0.05)
 
             # Envío de mensajes de temperatura al móvil
+            print("Enviando mensaje al móvil...")
             if T1 < 20:
-                tb.send_message(chatid, '¡¡Bienvenido a casa!!. La temperatura ambiente es de '+TStr + '. Creo que deberías encender la calefacción.')
+                tb.send_message(chatid, '¡¡Bienvenido a casa!! Hoy es '+ time.strftime ('%d %b y son las %H:%M. La temperatura ambiente es de '+TStr + ' ºC, por lo que creo que quizás deberías encender la calefacción.'))
 
             elif T1 > 32:
-                tb.send_message(chatid, '¡¡Bienvenido a casa!!. La temperatura ambiente es de '+TStr + '. Creo que deberías encender el aire acondicionado.')
+                tb.send_message(chatid, '¡¡Bienvenido a casa!! Hoy es '+ time.strftime ('%d %b y son las %H:%M. La temperatura ambiente es de '+TStr + ' ºC, por lo que creo que deberías encender el aire acondicionado.'))
 
             else:
-                tb.send_message(chatid, '¡¡Bienvenido a casa!!. La temperatura ambiente es de '+TStr + '. Creo que la temperatura ambiente es ideal.')
-                 
+                tb.send_message(chatid, '¡¡Bienvenido a casa!! Hoy es '+ time.strftime ('%d %b y son las %H:%M. La temperatura ambiente es de '+TStr + ' ºC, por lo que creo que la temperatura ambiente es ideal y no deberías encender ni la calefacción ni el aire acondicionado.'))
+                
+            print("Mensaje enviado\n")
+
+        # Si no es la ID que queremos no deja acceder a la casa  
         else:
             # Si no tiene esa ID la Sense Hat entera en rojo
             question_mark = [
@@ -121,10 +124,11 @@ while lectura_continua:
             time.sleep(1)
             
             # Acceso denegado por pantalla y Sense Hat
-            print "Error de autenticación\n"
+            print "Error de autenticación"
 
             # Hacemos foto para ver quién está intentando acceder.
             # Creamos objeto de la PiCámara y la cerramos al final
+            print "Realizando foto de seguridad..."
             camera = PiCamera()
             camera.resolution = (640,480)
             camera.rotation = 180
@@ -135,19 +139,22 @@ while lectura_continua:
             camera.capture('/home/pi/git/proyecto_asignatura/Proyecto/imagen.jpg')
             camera.stop_preview()
             camera.close()
-        
+            print "Foto realizada"
+            
+            # Mensaje por Sense Hat de acceso denegado
             sense.show_message('ACCESO DENEGADO',text_colour=[100,100,100], scroll_speed = 0.05)
 
             # Envío alerta de seguridad al móvil
-            tb.send_message(chatid, '[Alerta de Seguridad] Intento de acceso a su casa')
-            tb.send_message(chatid, 'Se ha recibido una alerta de que alguien sin permiso de autorización ha intentado acceder a su casa el' + time.strftime("%d %b %Y a las %H:%M:%S. Por favor, no responda a este mensaje, se trata de un mensaje automatizado y solo se ha enviado para informar de la alerta."))
+            print("Enviando mensaje al móvil...")
+            tb.send_message(chatid, '[Alerta de Seguridad] Intento de acceso a su casa.\n\nSe ha recibido una alerta de que alguien sin permiso de autorización ha intentado acceder a su casa el ' + time.strftime('%d %b %Y a las %H:%M:%S. A continuación adjuntamos una foto del individuo que ha intentado acceder a su casa:'))
             photo = open('/home/pi/git/proyecto_asignatura/Proyecto/imagen.jpg', 'rb')
             tb.send_photo(chatid, photo)
-
+            print("Mensaje enviado")
+            
             # Envio correo electrónico con aviso de seguridad y adjuntando la foto hecha
             server = smtplib.SMTP('smtp.gmail.com', 587)
             server.starttls()
-            server.login(direccion_fuente, "xxxxXXXXX")
+            server.login(direccion_fuente, "sdaaMIERA")
             msg = MIMEMultipart()
             msg['From'] = direccion_fuente
             msg['To'] = direccion_destino
@@ -170,6 +177,4 @@ while lectura_continua:
             except:
                 print "Error al enviar el correo"
                 server.quit()
-            print "\n"    
-
-    
+            print "\n"
